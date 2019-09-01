73346ad233e3b8d7d44f7f24f8d96abb03ce299e
Geroge Dan Miron <44091914+gdmiron@users.noreply.github.com>
2019-09-01 14:01:10 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,11 @@
 mkdir build
 cd build
 
+REM This is a fix for a CMake bug where it crashes because of the "/GL" flag
+REM See: https://gitlab.kitware.com/cmake/cmake/issues/16282
+set CXXFLAGS=%CXXFLAGS:-GL=%
+set CFLAGS=%CFLAGS:-GL=%
+
 cmake .. ^
     -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
