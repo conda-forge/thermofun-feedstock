243e0080747f04493985da8430f67d761753bdf9
Geroge Dan Miron <44091914+gdmiron@users.noreply.github.com>
2019-09-01 12:23:53 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,7 @@
 mkdir build
 cd build
 
-REM Configure step
-cmake -G ^
-      -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_INCLUDE_PATH:PATH="%LIBRARY_INC%" ^
-      -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
-      ..
-
-if errorlevel 1 exit 1
-
-REM Build!
-nmake
-if errorlevel 1 exit 1
-
-REM Install!
-nmake install
-if errorlevel 1 exit 1
+echo "Configuring..."
+cmake -S . -B build
+echo "Building..."
+cmake --build build --config %CONFIGURATION% --target install
