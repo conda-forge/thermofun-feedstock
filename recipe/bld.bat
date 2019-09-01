2b19ba41c6fbac99fc9925fa4d57ccef38be6af7
Geroge Dan Miron <44091914+gdmiron@users.noreply.github.com>
2019-09-01 18:11:32 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,8 +3,7 @@ cd build
 
 if "%VS_MAJOR%" == "9" (
 ECHO VS 2008
-) 
-else (
+) else (
 REM This is a fix for a CMake bug where it crashes because of the "/GL" flag
 REM See: https://gitlab.kitware.com/cmake/cmake/issues/16282
 set CXXFLAGS=%CXXFLAGS:-GL=%
