372076380bb033741f180d01a84b01a00f661a2e
Geroge Dan Miron <44091914+gdmiron@users.noreply.github.com>
2019-09-01 11:47:37 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,12 +2,12 @@ mkdir build
 cd build
 
 REM Configure step
-cmake .. ^
-    -GNinja ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_INCLUDE_PATH:PATH="%LIBRARY_INC%" ^
-    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
+cmake -G Ninja ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INCLUDE_PATH:PATH="%LIBRARY_INC%" ^
+      -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
+      ..
 
 REM Build step
 ninja install
