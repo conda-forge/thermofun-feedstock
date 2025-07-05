e6baf8f48d702b80f7934b1eec171012bc20135d
mirondanro@yahoo.com <mirondanro@yahoo.com>
2025-07-05 13:56:42 +0200

    build like chemicalfun

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,12 @@ mkdir build
 cd build
 
 cmake -G Ninja ^
-    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    ..
+      -DPYTHON_EXECUTABLE:FILEPATH="%PYTHON%" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INCLUDE_PATH:PATH="%LIBRARY_INC%" ^
+      ..
+
+if errorlevel 1 exit /b 1
 ninja install
+if errorlevel 1 exit /b 1
